a7993e41c77addeead306245d310b2699c544ac6
Dart_zynx <uthmanyas23@gmail.com>
2025-11-07 17:32:07 +0000

    message

--- modules/file_handler.py
+++ modules/file_handler.py
@@ -1,1 +1,34 @@
 import csv
+
+
+def read_data( file_name) :
+
+    cleaned_data = []      # this is where we store the cleaned data in
+
+    try : 
+        with open ( file_name , "r"  ) as file : 
+            content = csv.DictReader( file )
+            content.fieldnames = [ name.lower().strip() for name in content.fieldnames ] 
+
+            for row in content : 
+                cleaned_data.append ({
+                    "name" : row["name"] , 
+                    "math" : float(row["math"]) , 
+                    "english" : float(row["english"]) ,
+                    "science" : float(row["science"]) , 
+                    "history" : float(row["history"])
+                })
+
+            if not cleaned_data :
+                raise ValueError(" File is empty ")
+            
+            return cleaned_data
+
+
+
+    except FileNotFoundError :
+        print("File not Found")
+
+input_file = "raw_scores.csv"
+read_data(input_file)
+
