e71143218a8a050e547a1c42c4e3275fdd3072fd
Dart_zynx <uthmanyas23@gmail.com>
2025-11-16 17:09:27 +0000

    Half done with the report

--- modules/report_writer.py
+++ modules/report_writer.py
@@ -1,18 +1,35 @@
 # modules / report_writer.py
 
-def write_report (result , output_file) : 
-    try : 
+from grader import Student
+from file_handler import read_data
+import os
+import csv
 
-        with open ( output_file , "w") as file : 
 
-            file.write("Name\tAverage\tGrade\n")
-            file.write("="*30 + "\n")
+def process_scores(input_file) : 
+    show_data = read_data(input_file)
+    result = []
 
-            for name , avg , grade in result : 
-                file.write ( f"{name}\t{avg}\t{grade}\n" )
+    for name , scores in show_data : 
+        s = Student(name , scores)
+        s.calc_average()
+        s.assign_grade()
+        s.append(s)
 
-        print(f"Report successfully written to {output_file}")        
 
-    except Exception as e : 
-        print(f"Error writing report {e}")   
+# def write_report (result , output_file) : 
+#     try : 
+
+    #     with open ( output_file , "w") as file : 
+
+    #         file.write("Name\tAverage\tGrade\n")
+    #         file.write("="*30 + "\n")
+
+    #         for name , avg , grade in result : 
+    #             file.write ( f"{name}\t{avg}\t{grade}\n" )
+
+    #     print(f"Report successfully written to {output_file}")        
+
+    # except Exception as e : 
+    #     print(f"Error writing report {e}")   
         
