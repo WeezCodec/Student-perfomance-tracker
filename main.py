5acc516e01ae01d0feb987cdc4a6c8a1170b384f
Dart_zynx <uthmanyas23@gmail.com>
2025-11-19 11:23:17 +0000

    report_writer.py and main.py completed

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 # main.py 
 
-from modules.file_handler import read_data
-from modules.grader import process_students
-from modules.report_writer import write_report
+from modules.report_writer import process_scores
 
 def main() : 
 
     print("======== Student Perfomance Tracker ========")
     print("Processing ... Please wait ")
 
-    input_file = "/home/zynx/Documents/student-perfomance-tracker/data/raw_scores.csv"
-    output_file = "student-perfomance-tracker-results.txt"
-
-    show_data = read_data(input_file)
-    analyze_data = process_students(show_data)
-    write_report(analyze_data , output_file)
-
+process_scores("/home/zynx/Documents/student-perfomance-tracker/data/raw_scores.csv")
 
 
 if __name__  ==  "__main__" :
--- modules/report_writer.py
+++ modules/report_writer.py
@@ -8,28 +8,32 @@ import csv
 
 def process_scores(input_file) : 
     show_data = read_data(input_file)
+
     result = []
 
     for name , scores in show_data : 
         s = Student(name , scores)
         s.calc_average()
         s.assign_grade()
-        s.append(s)
+        result.append(s)
 
 
-# def write_report (result , output_file) : 
-#     try : 
+  # Ensures report folder exists
+    try : 
+        os.mkdir("reports" , exist_ok = True)
+        output_file = "reports/student-perfomance-tracker-results.csv"
 
-    #     with open ( output_file , "w") as file : 
+        with open(output_file , "w" , newline= "") as file : 
+            content = csv.writer ( file )
+            content.writerow(["Name" , "Average" , "Grade"])
+            
+            for s in Student : 
+                content.writerow([s.name , f"{s.average:.2f}", s.grade])
 
-    #         file.write("Name\tAverage\tGrade\n")
-    #         file.write("="*30 + "\n")
+        print(f"Report generated : {output_file}")         
 
-    #         for name , avg , grade in result : 
-    #             file.write ( f"{name}\t{avg}\t{grade}\n" )
+    except Exception as e : 
+        print("Error while writing to file {e}")
 
-    #     print(f"Report successfully written to {output_file}")        
-
-    # except Exception as e : 
-    #     print(f"Error writing report {e}")   
         
+
